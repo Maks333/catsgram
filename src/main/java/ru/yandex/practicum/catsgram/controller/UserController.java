b6ad89c28ce574428a59ed5acfc187e4be85d744
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-06-06 15:16:55 +0700

    Create POST /users endpoint
    This endpoint handles addition of new users into the
    system. It checks that new users have email and if new
    emails do not present in the system. New users get
    registration date and id. getNextId function generate next
    max id for users.

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,10 +1,11 @@
 package ru.yandex.practicum.catsgram.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
 import ru.yandex.practicum.catsgram.model.User;
 
+import java.time.Instant;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,5 +18,35 @@ public class UserController {
     @GetMapping
     public Collection<User> findAll() {
         return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        if (user.getEmail() == null || user.getEmail().isBlank()) {
+            throw new ConditionsNotMetException("Email must be present");
+        }
+
+        users.values()
+                .stream()
+                .map(User::getEmail)
+                .filter(email -> email.equals(user.getEmail()))
+                .findFirst()
+                .ifPresent(email -> {
+                    throw new DuplicatedDataException("Email " + email + " is already in use");
+                });
+
+        user.setId(getNextId());
+        user.setRegistrationDate(Instant.now());
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    private Long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
     }
 }
