f6ca1ee9b2b5ac074548672ba513661a6b4a4432
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-06-14 13:11:34 +0700

    Refactor UserController
    Inject UserService dependency into UserController and remove excess code.

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,84 +1,32 @@
 package ru.yandex.practicum.catsgram.controller;
 
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
-import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
-import ru.yandex.practicum.catsgram.exception.NotFoundException;
 import ru.yandex.practicum.catsgram.model.User;
+import ru.yandex.practicum.catsgram.service.UserService;
 
-import java.time.Instant;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final Map<Long, User> users = new HashMap<>();
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public Collection<User> findAll() {
-        return users.values();
+        return userService.findAll();
     }
 
     @PostMapping
     public User create(@RequestBody User user) {
-        if (user.getEmail() == null || user.getEmail().isBlank()) {
-            throw new ConditionsNotMetException("Email must be present");
-        }
-
-        users.values()
-                .stream()
-                .map(User::getEmail)
-                .filter(email -> email.equals(user.getEmail()))
-                .findFirst()
-                .ifPresent(email -> {
-                    throw new DuplicatedDataException("Email " + email + " is already in use");
-                });
-
-        user.setId(getNextId());
-        user.setRegistrationDate(Instant.now());
-        users.put(user.getId(), user);
-        return user;
+        return userService.create(user);
     }
 
     @PutMapping
     public User update(@RequestBody User user) {
-        if (user.getId() == null) {
-            throw new ConditionsNotMetException("Id must be present");
-        }
-
-        if (users.containsKey(user.getId())) {
-            User userToUpdate = users.get(user.getId());
-            if (user.getEmail() != null &&
-                    !user.getEmail().isBlank() &&
-                    !userToUpdate.getEmail().equals(user.getEmail())) {
-                users.values()
-                        .stream()
-                        .map(User::getEmail)
-                        .filter(email -> email.equals(user.getEmail()))
-                        .findFirst()
-                        .ifPresentOrElse(email -> {
-                            throw new DuplicatedDataException("Email " + email + " is already in use");
-                        }, () -> userToUpdate.setEmail(user.getEmail()));
-            }
-            if (user.getPassword() != null) {
-                userToUpdate.setPassword(user.getPassword());
-            }
-            if (user.getUsername() != null) {
-                userToUpdate.setUsername(user.getUsername());
-            }
-            return userToUpdate;
-        }
-        throw new NotFoundException("User with id = " + user.getId() + " is not found");
-    }
-
-    private Long getNextId() {
-        long currentMaxId = users.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++currentMaxId;
+        return userService.update(user);
     }
 }
