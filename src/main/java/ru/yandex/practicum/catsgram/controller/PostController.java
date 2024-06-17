3c7e4a77e46f2bebb165587351e25e827ed04ca6
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-06-17 15:04:57 +0700

    Modify findAll function
    Now function return list of selective posts, that sorted in descending or ascending order.

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -21,8 +21,12 @@ public class PostController {
     }
 
     @GetMapping
-    public Collection<Post> findAll() {
-        return postService.findAll();
+    public Collection<Post> findAll(
+            @RequestParam(defaultValue = "0") int from,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "desc") String sort
+    ) {
+        return postService.findAll(from, size, sort);
     }
 
     @PostMapping
