72efa74d517d48e3abb4d963074b670929381286
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-06-17 17:44:17 +0700

    Create getPostImages function
    Create GET /posts/{postId}/images endpoint, that returns images of specified post

--- src/main/java/ru/yandex/practicum/catsgram/controller/ImageController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/ImageController.java
@@ -1,12 +1,22 @@
 package ru.yandex.practicum.catsgram.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.catsgram.model.Image;
 import ru.yandex.practicum.catsgram.service.ImageService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class ImageController {
     private final ImageService imageService;
-    
+
+    @GetMapping("/posts/{postId}/images")
+    public List<Image> getPostImages(@PathVariable long postId) {
+        return imageService.getPostImages(postId);
+    }
 }
