470b506a2090001237ac89eabef0ae32a5d2be0b
Maxim Myasnenko <MaxChip322@yandex.ru>
2024-06-21 16:49:43 +0700

    Create ErrorResponse
    Inner class that represent error response

--- src/main/java/ru/yandex/practicum/catsgram/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/ErrorHandler.java
@@ -4,5 +4,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ErrorHandler {
+    private static class ErrorResponse {
+        private final String error;
+
+        public ErrorResponse(String error) {
+            this.error = error;
+        }
+    }
 
 }
